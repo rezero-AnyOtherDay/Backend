e4a96f4ae890474bac09ced9370ace81722a9e5b
ddol9 <drddol9@gmail.com>
2025-11-12 17:48:43 +0900

    first commit

---
+++ src/main/java/com/rezero/anyotherday/AnyotherdayApplication.java
@@ -0,0 +1,13 @@
+package com.rezero.anyotherday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyotherdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyotherdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rezero/anyotherday/AnyotherdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezero.anyotherday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyotherdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
