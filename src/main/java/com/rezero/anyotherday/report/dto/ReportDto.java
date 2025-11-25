63f9060d9e39fe179c9e7bb965135b5c85be9539
ddol9 <drddol9@gmail.com>
2025-11-25 17:32:31 +0900

    update schema

--- src/main/java/com/rezero/anyotherday/report/dto/ReportDto.java
+++ src/main/java/com/rezero/anyotherday/report/dto/ReportDto.java
@@ -1,32 +1,20 @@
 package com.rezero.anyotherday.report.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class ReportDto {
-
-    @JsonProperty("report_id")
     private Integer reportId;
-
-    @JsonProperty("record_id")
     private Integer recordId;
-
-    @JsonProperty("analysis_result")
-    private String analysisResult;  // JSON 문자열로 저장
-
-    @JsonProperty("created_at")
+    private String analysisResult;
     private LocalDateTime createdAt;
-
-    @JsonProperty("updated_at")
     private LocalDateTime updatedAt;
 }
