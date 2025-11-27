7041ebc21ab1872ea7f413d60a5f37265798f34b
hepa <akfalwkfalwkf@gmail.com>
2025-11-27 15:02:42 +0900

    메세지

--- src/main/java/com/rezero/anyotherday/AnyotherdayApplication.java
+++ src/main/java/com/rezero/anyotherday/AnyotherdayApplication.java
@@ -2,8 +2,10 @@ package com.rezero.anyotherday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:.env", ignoreResourceNotFound = true)
 public class AnyotherdayApplication {
 
 	public static void main(String[] args) {
